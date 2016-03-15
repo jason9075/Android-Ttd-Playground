001bfaa9b21e1819fe7879767998454c649c667b
Capillary J <jason9075@gmail.com>
2016-03-15 12:23:20 +0800

    add "MainActivityTest" test

---
+++ app/src/androidTest/java/com/jason9075/androidttdplayground/MainActivityTest.java
@@ -0,0 +1,39 @@
+package com.jason9075.androidttdplayground;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void initValidString() {
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        onView(withId(R.id.result_textview))
+                .check(matches(withText("Hello World!")));
+        onView(withId(R.id.change_text_button))
+                .perform(click());
+        onView(withId(R.id.result_textview))
+                .check(matches(withText("Yo!")));
+    }
+}
