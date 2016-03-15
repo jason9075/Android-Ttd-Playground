f3485e6293a67f946802f00c87a5667e27044b23
Capillary J <jason9075@gmail.com>
2016-03-15 12:32:05 +0800

    implement MainActivity and pass "MainActivityTest" test

--- app/src/main/java/com/jason9075/androidttdplayground/MainActivity.java
+++ app/src/main/java/com/jason9075/androidttdplayground/MainActivity.java
@@ -2,12 +2,26 @@ package com.jason9075.androidttdplayground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView resultTextview;
+    private Button changeTextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTextview = (TextView) findViewById(R.id.result_textview);
+        changeTextButton = (Button) findViewById(R.id.change_text_button);
+
+        changeTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultTextview.setText("Yo!");
+            }
+        });
     }
 }
