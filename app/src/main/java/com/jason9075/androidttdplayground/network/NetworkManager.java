70b514dd07151a7aa77bfb722853c4fecbf602da
Capillary J <jason9075@gmail.com>
2016-03-15 15:58:51 +0800

    implement NetworkManager.class and pass NetworkManagerTest

--- app/src/main/java/com/jason9075/androidttdplayground/network/NetworkManager.java
+++ app/src/main/java/com/jason9075/androidttdplayground/network/NetworkManager.java
@@ -4,11 +4,13 @@ package com.jason9075.androidttdplayground.network;
  * Created by jason9075 on 2016/3/15.
  */
 public class NetworkManager {
-    public NetworkManager(GithubService githubService) {
+    private GithubService githubService;
 
+    public NetworkManager(GithubService githubService) {
+        this.githubService = githubService;
     }
 
     public void userCheck() {
-
+        githubService.userCheck();
     }
 }
