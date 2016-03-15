62a44c810cb7fcd8af31c3cbc5c0607624bb912e
Capillary J <jason9075@gmail.com>
2016-03-15 13:07:52 +0800

    implement MainActivity and pass githubUserEmptyCheckTest test

--- app/src/main/java/com/jason9075/androidttdplayground/MainActivity.java
+++ app/src/main/java/com/jason9075/androidttdplayground/MainActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView resultTextview;
     private Button changeTextButton;
+    private EditText accountEdittext;
+    private TextView githubResultTextview;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         resultTextview = (TextView) findViewById(R.id.result_textview);
         changeTextButton = (Button) findViewById(R.id.change_text_button);
+        accountEdittext = (EditText) findViewById(R.id.account_edittext);
+        githubResultTextview = (TextView) findViewById(R.id.submit_result_textview);
+        submitButton = (Button) findViewById(R.id.submit_button);
 
         changeTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 resultTextview.setText("Yo!");
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(accountEdittext.getText().toString().equals(""))
+                    githubResultTextview.setText("account is empty!");
             }
         });
     }
